ee22cf1a0d1a5f9d93f67681bd3186ab0c6454e6
Weston Gorczyca <weston.gorczyca@gmail.com>
2026-06-07 20:41:47 -0400

    added some ast nodes

---
+++ ast.h
@@ -0,0 +1,27 @@
+#ifndef AST_H
+#define AST_H
+#include <memory>
+#include <string>
+
+using namespace std;
+
+
+class ASTNode {
+    public:
+    ASTNode(){}
+    virtual ~ASTNode() = default;
+};
+
+class ASTExp : public ASTNode {};
+
+class BinExp : public ASTExp {
+    public:
+    BinExp(unique_ptr<ASTExp> left, unique_ptr<ASTExp> right, string op) : left(move(left)), right(move(right)), op(op) {};
+    
+    private:
+    unique_ptr<ASTExp> left;
+    unique_ptr<ASTExp> right;
+    string op;
+};
+
+#endif // AST_H
